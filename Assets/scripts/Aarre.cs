91901ced122edc867ced1e08dab1914a93311f7c
Someone Someone <someone@example.com>
2021-05-14 18:20:22 +0300

    törmäys

---
+++ Assets/scripts/Aarre.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aarre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("Löysit aarteen!");
+        }
+        //if (collision.gameObject.name == "AMMUS")
+       // {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+         //   Debug.Log("Löysit aarteen!");
+        //}
+    }
+}
